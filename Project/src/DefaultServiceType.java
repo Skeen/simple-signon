53dd2a347eb307f12e524cac4fd9d688d0190338
Emil Madsen <skeen@cs.au.dk>
2013-04-30 14:35:03 +0200

    Added service types

---
+++ Project/src/DefaultServiceType.java
@@ -0,0 +1,17 @@
+class DefaultServiceType implements ServiceType
+{
+    public boolean isConnected()
+    {
+        return false;
+    }
+
+    public boolean connect()
+    {
+        return false;
+    }
+
+    public boolean disconnect()
+    {
+        return false;
+    }
+}
