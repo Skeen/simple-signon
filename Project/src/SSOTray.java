0411fe272b03ab094b07cb8756c53274aa53da45
Emil Madsen <skeen@cs.au.dk>
2013-04-30 13:57:10 +0200

    Additional cleanup

---
+++ Project/src/SSOTray.java
@@ -0,0 +1,122 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import javax.swing.*;
+
+class SSOTray
+{
+    private static SSOTray singleton;
+    public static SSOTray getSingleton()
+    {
+        if(singleton == null)
+        {
+            singleton = new SSOTray();
+        }
+        return singleton;
+    }
+
+    private static final String SystemTrayPath = "resource/SystemTray.png";
+
+    private SSOTray()
+    {
+        create();
+    }
+
+    private void create() 
+    {
+        // Check the SystemTray support
+        if (!SystemTray.isSupported()) 
+        {
+            System.err.println("SystemTray is not supported");
+            return;
+        }
+        final PopupMenu popup = new PopupMenu();
+        final TrayIcon trayIcon = new TrayIcon(Utilities.loadImage(SystemTrayPath));// "Tray Icon"));
+        trayIcon.setImageAutoSize(true);
+        trayIcon.setToolTip("Simple-Signon SystemTray");
+
+        final SystemTray tray = SystemTray.getSystemTray();
+
+        // Create a popup menu components
+        MenuItem aboutItem = new MenuItem("About");
+        Menu displayMenu = new Menu("Display");
+        MenuItem errorItem = new MenuItem("Error");
+        MenuItem warningItem = new MenuItem("Warning");
+        MenuItem infoItem = new MenuItem("Info");
+        MenuItem noneItem = new MenuItem("None");
+        MenuItem exitItem = new MenuItem("Exit");
+
+        //Add components to popup menu
+        popup.add(aboutItem);
+        popup.addSeparator();
+        popup.add(displayMenu);
+        displayMenu.add(errorItem);
+        displayMenu.add(warningItem);
+        displayMenu.add(infoItem);
+        displayMenu.add(noneItem);
+        popup.add(exitItem);
+
+        trayIcon.setPopupMenu(popup);
+
+        try {
+            tray.add(trayIcon);
+        } catch (AWTException e) {
+            System.err.println("TrayIcon could not be added.");
+            return;
+        }
+
+        trayIcon.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                    "This dialog box is run from System Tray");
+            }
+        });
+
+        aboutItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,
+                    "This dialog box is run from the About menu item");
+            }
+        });
+
+        ActionListener listener = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                MenuItem item = (MenuItem)e.getSource();
+                //TrayIcon.MessageType type = null;
+                System.out.println(item.getLabel());
+                if ("Error".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.ERROR;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an error message", TrayIcon.MessageType.ERROR);
+
+                } else if ("Warning".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.WARNING;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is a warning message", TrayIcon.MessageType.WARNING);
+
+                } else if ("Info".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.INFO;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an info message", TrayIcon.MessageType.INFO);
+
+                } else if ("None".equals(item.getLabel())) {
+                    //type = TrayIcon.MessageType.NONE;
+                    trayIcon.displayMessage("Sun TrayIcon Demo",
+                            "This is an ordinary message", TrayIcon.MessageType.NONE);
+                }
+            }
+        };
+
+        errorItem.addActionListener(listener);
+        warningItem.addActionListener(listener);
+        infoItem.addActionListener(listener);
+        noneItem.addActionListener(listener);
+
+        exitItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                tray.remove(trayIcon);
+                System.exit(0);
+            }
+        });
+    }
+}
