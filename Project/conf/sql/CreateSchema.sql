65dadcac7720e3a50ec6d827189f60ba596189ba
Emil Madsen <skeen@cs.au.dk>
2013-05-22 14:20:37 +0200

    We're now loading services from the sql server

--- Project/conf/sql/CreateSchema.sql
+++ Project/conf/sql/CreateSchema.sql
@@ -1,13 +1,14 @@
-delimiter $$
+DROP DATABASE IF EXISTS `services`; 
 
-CREATE DATABASE `servies` /*!40100 DEFAULT CHARACTER SET latin1 */$$
+CREATE DATABASE `services`;
+USE `services`;
 
 CREATE TABLE `service_type` (
   `idservice_type` int(11) NOT NULL,
   `service_type_name` varchar(64) NOT NULL,
   #`implementation` varchar(45) NOT NULL,
   PRIMARY KEY (`idservice_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `service` (
   `idservice` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,7 +22,7 @@ CREATE TABLE `service` (
   KEY `service_type_must_exist_idx` (`service_type_id`),
   CONSTRAINT `service_type_must_exist` FOREIGN KEY (`service_type_id`) REFERENCES `service_type` (`idservice_type`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `dependency_must_exist` FOREIGN KEY (`dependency_id`) REFERENCES `service` (`idservice`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `user_service` (
   `user_service_id` int(11) NOT NULL,
@@ -31,7 +32,7 @@ CREATE TABLE `user_service` (
   KEY `service_must_exist_idx` (`service_id`),
   KEY `username_index` (`username`),
   CONSTRAINT `service_must_exist` FOREIGN KEY (`service_id`) REFERENCES `service` (`idservice`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 # TODO: Contraint, such that one of (service_id and user_service_id) is null, and the otherone isn't.
 CREATE TABLE `service_indirection` (
@@ -43,7 +44,7 @@ CREATE TABLE `service_indirection` (
   KEY `user_service_id_must_exist_idx` (`user_service_id`),
   CONSTRAINT `user_service_id_must_exist` FOREIGN KEY (`user_service_id`) REFERENCES `user_service` (`user_service_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `service_id_must_exist` FOREIGN KEY (`service_id`) REFERENCES `service` (`idservice`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `key_value` (
   `service_indirection_id` int(11) NOT NULL,
@@ -51,4 +52,5 @@ CREATE TABLE `key_value` (
   `value` varchar(256) NOT NULL,
   PRIMARY KEY (`service_indirection_id`, `key`),
   CONSTRAINT `1service_id_must_exist` FOREIGN KEY (`service_indirection_id`) REFERENCES `service_indirection` (`idservice_indirection`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
--- Project/conf/sql/DataInsert.sql
+++ Project/conf/sql/DataInsert.sql
@@ -1,18 +1,21 @@
-INSERT INTO `servies`.`service_type` (`idservice_type`, `service_type_name`) VALUES ('1', '\"EXECUTE_PROGRAM\"');
-INSERT INTO `servies`.`service_type` (`idservice_type`, `service_type_name`) VALUES ('2', '\"HTML_FORM\"');
+USE `services`;
 
-INSERT INTO `servies`.`service` (`idservice`, `service_type_id`, `service_name`) VALUES ('1', '1', '\"CISCO_VPN\"');
-INSERT INTO `servies`.`service` (`idservice`, `dependency_id`, `service_type_id`, `service_name`) VALUES ('2', '1', '2', '\"IT\'S LEARNING\"');
+INSERT INTO `services`.`service_type` (`idservice_type`, `service_type_name`) VALUES ('1', '\"EXECUTE_PROGRAM\"');
+INSERT INTO `services`.`service_type` (`idservice_type`, `service_type_name`) VALUES ('2', '\"HTML_FORM\"');
 
-INSERT INTO `servies`.`user_service` (`user_service_id`, `username`, `service_id`) VALUES ('1', 'skeen', '1');
-INSERT INTO `servies`.`user_service` (`user_service_id`, `username`, `service_id`) VALUES ('2', 'skeen', '2');
+INSERT INTO `services`.`service` (`idservice`, `service_type_id`, `service_name`) VALUES ('1', '1', '\"CISCO_VPN\"');
+INSERT INTO `services`.`service` (`idservice`, `dependency_id`, `service_type_id`, `service_name`) VALUES ('2', '1', '2', '\"IT\'S LEARNING\"');
 
-INSERT INTO `servies`.`service_indirection` (`idservice_indirection`, `service_id`) VALUES ('1', '1');
-INSERT INTO `servies`.`service_indirection` (`idservice_indirection`, `service_id`) VALUES ('2', '2');
-INSERT INTO `servies`.`service_indirection` (`idservice_indirection`, `user_service_id`) VALUES ('3', '1');
-INSERT INTO `servies`.`service_indirection` (`idservice_indirection`, `user_service_id`) VALUES ('4', '2');
+INSERT INTO `services`.`user_service` (`user_service_id`, `username`, `service_id`) VALUES ('1', 'skeen', '1');
+INSERT INTO `services`.`user_service` (`user_service_id`, `username`, `service_id`) VALUES ('2', 'skeen', '2');
 
-INSERT INTO `servies`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('1', 'PATH', 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe connect user ${USERNAME} pwd ${PASSWORD}');
-INSERT INTO `servies`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('2', 'WEB_PAGE', 'www.login.itslearning.dk');
-INSERT INTO `servies`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('3', 'USERNAME', 'skeen');
-INSERT INTO `servies`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('3', 'PASSWORD', '12345');
+INSERT INTO `services`.`service_indirection` (`idservice_indirection`, `service_id`) VALUES ('1', '1');
+INSERT INTO `services`.`service_indirection` (`idservice_indirection`, `service_id`) VALUES ('2', '2');
+INSERT INTO `services`.`service_indirection` (`idservice_indirection`, `user_service_id`) VALUES ('3', '1');
+INSERT INTO `services`.`service_indirection` (`idservice_indirection`, `user_service_id`) VALUES ('4', '2');
+
+INSERT INTO `services`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('1', 'PATH', 'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe connect user ${USERNAME} pwd ${PASSWORD}');
+INSERT INTO `services`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('2', 'WEB_PAGE', 'www.login.itslearning.dk');
+INSERT INTO `services`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('3', 'USERNAME', 'skeen');
+INSERT INTO `services`.`key_value` (`service_indirection_id`, `key`, `value`) VALUES ('3', 'PASSWORD', '12345');
+
