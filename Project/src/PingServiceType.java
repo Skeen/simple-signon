4866919b607e497a4a0235aa76e2343858094171
Emil Madsen <skeen@cs.au.dk>
2013-05-27 18:51:52 +0200

    Use-case done

---
+++ Project/src/PingServiceType.java
@@ -0,0 +1,77 @@
+import java.io.*;
+
+import java.util.Map;
+
+class PingServiceType implements ServiceType
+{
+    private volatile Boolean connected = null;
+    public PingServiceType(Map<String,String> input)
+    {
+        final String host = input.get("host");
+        new Thread(new Runnable()
+                {
+                    public void run() {
+                        while(true) {
+                            connected = ping(host);
+                        }
+                    }
+                }).start();
+    }
+
+    public void double_click()
+    {
+    }
+
+    private static boolean ping(String host)
+    {
+        boolean isReachable = false;
+        try
+        {
+            Process proc = new ProcessBuilder("ping"/*, "-n 1"*/, host).start();
+
+            int exitValue = proc.waitFor();
+            //System.out.println("Exit Value:" + exitValue);
+            if(exitValue == 0)
+            {
+                isReachable = true;
+            }
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        return isReachable;
+    }
+    
+    public Service.Status getStatus()
+    {
+        // If we got no ping report yet, let's wait for it
+        if(connected == null)
+        {
+            return Service.Status.CONNECTING;
+        }
+        // Are we connected
+        else if(connected)
+        {
+            return Service.Status.CONNECTED;
+        }
+        // We are not connecting, we must be disconnected
+        else
+        {
+            return Service.Status.DISCONNECTED;
+        }
+    }
+
+    public void connect()
+    {
+    }
+
+    public void disconnect()
+    {
+    }
+}
---
+++ Project/src/Utilities.java
@@ -0,0 +1,97 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import javax.imageio.ImageIO;
+
+class Utilities
+{
+    private static class ImageUtil
+    {
+        private static Map<String, Image> image_map = new HashMap<String, Image>();
+
+        // Loads an image, or returns the cached one
+        public static Image load(String path)
+        {
+            // Try to get the image from the map
+            Image image = image_map.get(path);
+            if(image == null)
+            {
+                try
+                {
+                    // Load it
+                    image = ImageIO.read(new File(path));
+                    // ... and add it to the map
+                    image_map.put(path, image);
+                }
+                catch(IOException e)
+                {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+            // Return it
+            return image;
+        }
+    }
+
+    public static Image loadImage(String path)
+    {
+        return ImageUtil.load(path);
+    }
+
+    public static Image overlayImage(Image background, Image overlay)
+    {
+        Dimension dim = new Dimension(background.getHeight(null), background.getWidth(null));
+        // Creates the buffered image.
+        BufferedImage buffImg1 = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g1 = buffImg1.createGraphics();
+
+        // Creates the buffered image.
+        BufferedImage buffImg2 = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2 = buffImg2.createGraphics();
+
+        // Clears the previously drawn image.
+        g1.setColor(Color.white);
+        g1.fillRect(0, 0, dim.width, dim.height);
+
+        // Draws the rectangle and ellipse into the buffered image.
+        g2.drawImage(background, null, null);
+
+        AffineTransform transform = new AffineTransform();
+        transform.setToTranslation(dim.height-overlay.getHeight(null), dim.width-overlay.getWidth(null));
+        g2.drawImage(overlay, transform, null);
+
+        // Draws the buffered image.
+        g1.drawImage(buffImg2, null, 0, 0);
+
+        return buffImg2;
+    }
+
+    public static int getRandomBetween(int min, int max)
+    {
+        Random rand = new Random();
+        return min + rand.nextInt(max - min + 1);
+    }
+
+    public static void delay(int amount)
+    {
+        try
+        {
+            // Just sleep
+            Thread.sleep(amount);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
